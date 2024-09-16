12001a29a5c4a997422254bc92fcabe125d4c9ef
NHTPhuong35 <thanhphuong3504@gmail.com>
2024-09-16 22:45:37 +0700

    first commit

---
+++ src/BUS/NhanVienBUS.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BUS;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class NhanVienBUS {
+     public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
