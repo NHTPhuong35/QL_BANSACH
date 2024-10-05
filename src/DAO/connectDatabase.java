5966b1bf78b8add8ca347a602a142515a08340c1
unknown <quynhdinh704@gmail.com>
2024-10-05 10:41:53 -1200

    pull main x1

--- src/DAO/connectDatabase.java
+++ src/DAO/connectDatabase.java
@@ -21,7 +21,7 @@ public class connectDatabase {
         dbName = "qlbs";
         driver = "com.mysql.cj.jdbc.Driver";
         userName = "root";
-        password = "Phuong35";
+        password = "quynh04";
 
     }
 
